9414555a831938b6cbbdce6c835444dd9f41b8d7
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-23 14:42:26 +0800

    v2117.1.0.0
    Lime3DS 2116 -> 2117.1

--- install_pkg.sh
+++ install_pkg.sh
@@ -3,15 +3,16 @@ set -e
 
 function help() {
     echo "usage:"
-    echo "$(basename $0) -i -d DEB -p PATH [-I INCLUDE] [-E EXCLUDE] [-L LOGS]"
+    echo "$(basename $0) -i -d DEB -p PATH [-R] [-I INCLUDE] [-E EXCLUDE] [-L LOGS]"
     echo "$(basename $0) -u -r REGEX [-L LOG] [-D]"
     echo "    -i 安装deb包"
     echo "    -u 删除已安装的deb包"
     echo "    -d DIR 待安装deb包目录"
     echo "    -r '\-dev|pkg1|pkg2...' 需要删除的deb包的正则表达式"
     echo "    -p PATH 安装路径"
+    echo "    -R 安装时修复RUNPATH"
     echo "    -I 'pkg1,pkg2...' 需要强制安装的deb包列表, 以逗号分隔, 不受-E影响"
-    echo "    -E 'pkg1,pkg2...' 需要排除的deb包列表, 以逗号分隔"
+    echo "    -E 'pkg1,pkg2...'|'*' 需要排除的deb包列表, 以逗号分隔, 参数为'*'时排除所有包"
     echo "    -L LOGS 安装记录存放目录"
     echo "    -D 删除安装记录"
     echo "    -h 打印帮助信息"
@@ -39,6 +40,7 @@ function install() {
     skipped_list="$deb_trace_dir/0_skipped.list"
     mkdir $deb_trace_dir
     touch $skipped_list
+    echo "-----${temp_dir##*.}-----" > $skipped_list
     # 生成文件列表
     find "$deb_dir" -type f -name "*.deb" >"$deb_list_file"
     echo "$include" | tr ',' '\n' >"$include_list_file"
@@ -63,8 +65,8 @@ function install() {
         # 获取包名
         pkg=$(tar -xf "$control_file" ./control -O | grep '^Package:' | awk '{print $2}')
         rm "$control_file"
-        # 如果包含在exclude列表, 并且不包含在include列表则跳过安装
-        if grep -q "^$pkg$" "$exclude_list_file" && ! grep -q "^$pkg$" "$include_list_file"; then
+        # 如果exclude中含星号*或deb包含在exclude列表中, 且不包含在include列表则跳过安装
+        if grep -q "^$pkg$\|*" "$exclude_list_file" && ! grep -q "^$pkg$" "$include_list_file"; then
             echo " skip"
             echo "$file" >>$skipped_list
         else
@@ -88,7 +90,7 @@ function install() {
             # 修改指向/lib的绝对路径的软链接
             find "$data_list_dir" -type l | while IFS= read -r file; do
                 linkTarget=$(readlink "$file")
-                # 如果指向的路径以/lib开头, 并且文件不存在, 则添加 $path 前缀
+                # 如果指向的路径以/lib开头, 并且文件不存在, 则添加 $ins_path 前缀
                 # 部分 dev 包会创建 so 文件的绝对链接指向 /lib 目录下
                 # + 处理 /etc
                 if echo "$linkTarget" | grep -q '^/lib\|^/etc' && ! [ -f "$linkTarget" ]; then
@@ -109,10 +111,13 @@ function install() {
                     echo "    FIX RUNPATH" "$file" "$runpath" "=>" "$newRunpath"
                 fi
                 # +
-                newRunpath="$ins_path/lib/$TRIPLET"
-                if ldd "$file" | grep -q 'not found'; then
-                    patchelf --set-rpath "$newRunpath" "$file"
-                    echo "    FIX RUNPATH" "$file" "$newRunpath"
+                # 如果启用了 -R 选项则修复 RUNPATH
+                if [ -n "$rpath" ]; then
+                    newRunpath="$ins_path/lib/$TRIPLET"
+                    if ldd "$file" | grep -q 'not found'; then
+                        patchelf --set-rpath "$newRunpath" "$file"
+                        echo "    FIX RUNPATH" "$file" "$newRunpath"
+                    fi
                 fi
                 # +/
             done
@@ -129,16 +134,18 @@ function install() {
             # 复制/lib,/bin,/usr目录
             # + 复制/etc目录
             mkdir -p "$ins_path"
-            cp -rP "$data_list_dir/lib" "$path" 2>/dev/null || true
-            cp -rP "$data_list_dir/bin" "$path" 2>/dev/null || true
-            cp -rP "$data_list_dir/etc" "$path" 2>/dev/null || true
-            cp -rP "$data_list_dir"/usr/* "$path" || true
+            cp -rP "$data_list_dir"/lib   "$ins_path" 2>/dev/null || true
+            cp -rP "$data_list_dir"/bin   "$ins_path" 2>/dev/null || true
+            cp -rP "$data_list_dir"/etc   "$ins_path" 2>/dev/null || true
+            cp -rP "$data_list_dir"/usr/* "$ins_path" || true
             rm -r "$data_list_dir"
         fi
     done <"$deb_list_file"
 
     # 复制安装记录
-    cp -r "$deb_trace_dir" "$logs"
+    mkdir -p "$logs"
+    cat "$skipped_list" >> "$logs/$(basename $skipped_list)"
+    cp -r "$deb_trace_dir"/*.deb.list "$logs" 2>/dev/null || true
     # 清理临时目录
     rm -r "$temp_dir"
 }
@@ -172,16 +179,17 @@ function uninstall() {
     fi
 }
 
-unset -v mode deb_dir path include exclude logs regex del_log
+unset -v mode deb_dir path rpath include exclude logs regex del_log
 
-while getopts 'hiud:p:I:E:L:r:D' OPT; do
+while getopts 'hiud:p:RI:E:L:r:D' OPT; do
     case $OPT in
     i) mode="install" ;;
     u) mode="uninstall" ;;
     d) deb_dir="$OPTARG" ;;
     p) path="$OPTARG" ;;
-    I) include=${OPTARG//\"/} ;;
-    E) exclude=${OPTARG//\"/} ;;
+    R) rpath="true" ;;
+    I) include="${OPTARG//\"/}" ;;
+    E) exclude="${OPTARG//\"/}" ;;
     L) logs="$OPTARG" ;;
     r) regex="$OPTARG" ;;
     D) del_log="true" ;;
